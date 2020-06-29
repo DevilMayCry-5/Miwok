b79bcff2d88a35acbdecba42e50d0b568141b3d9
pho <phonum5@gmail.com>
2020-06-29 18:31:40 +0000

    modify NumbersActivity and PhrasesActivity to use the Word class to load audio file. add code to release system resource when audio resouce not in use

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,18 +5,21 @@ public class Word {
     private int mImage = NO_IMAGE;
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mAudioResourceId;
 
     private static final int NO_IMAGE = -1;
 
-    public Word(String DefaultTranslation, String MiwokTranslation, int Image){
+    public Word(String DefaultTranslation, String MiwokTranslation, int Image, int AudioResourceId){
         mDefaultTranslation = DefaultTranslation;
         mMiwokTranslation = MiwokTranslation;
         mImage = Image;
+        mAudioResourceId = AudioResourceId;
     }
 
-    public Word(String DefaultTranslation, String MiwokTranlation){
+    public Word(String DefaultTranslation, String MiwokTranlation, int AudioResourceId){
         mDefaultTranslation = DefaultTranslation;
         mMiwokTranslation = MiwokTranlation;
+        mAudioResourceId = AudioResourceId;
 
     }
 
@@ -37,5 +40,19 @@ public class Word {
 
     public boolean hasImage(){
         return mImage != NO_IMAGE;
+    }
+
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mImage=" + mImage +
+                ", mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mAudioResourceId=" + mAudioResourceId +
+                '}';
     }
 }
